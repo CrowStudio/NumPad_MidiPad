888752ac13b7faaddfb5bb2c5541170443986129
CrowStudio <crowstudio@gmail.com>
2022-05-22 18:52:52 +0200

    Improved pixel color handling, and added row color

--- code.py
+++ code.py
@@ -12,17 +12,17 @@ CYAN = (0, 255, 255)
 MAGENTA = (255, 0, 255)
 MINT = (0, 255, 50)
 
-BKG_COLOR = MAGENTA
+BKGND_COLOR = MAGENTA
 PRESSED_COLOR = CYAN
 
-CC_NUM0 = 7  # Volume CC number
-CC_NUM1 = 10 # Pan CC number
-CC_NUM2 = 1  # Modulation Wheel CC number
+CC_NUM0 = 7  # Volume
+CC_NUM1 = 10  # Pan
+CC_NUM2 = 1  # Modulation Wheel
 
-# create the macropad object, rotate orientation
+# create the macropad object, rotate orientation, set state for startup config
 macropad = MacroPad(rotation=0)
 macropad.display.auto_refresh = False  # avoid lag
-config_mode = 1  # state for startup config
+config_mode = 1
 
 # --- Pixel setup --- #
 macropad.pixels.brightness = 0.05
@@ -35,8 +35,9 @@ text_lines[0].text = "Yellow = NumPad"
 text_lines[1].text = "Magenta = BlackBox"
 text_lines.show()
 
-# --- Character setup --- #
 encoder_map = ["+", "-", "*", "/", "(", ")", "%", "<-", ".", "="]
+
+row = [3,4]
 
 key_map = [['7', '8', '9',
             '4', '5', '6',
@@ -46,6 +47,15 @@ key_map = [['7', '8', '9',
             44, 45, 46,
             40, 41, 42,
             36, 37, 38]]
+
+row_map = [[48, 49, 50,
+           44, 45, 46,
+           40, 41, 42,
+           36, 37, 38],
+           [48, 49, 51,
+           44, 45, 47,
+           40, 41, 43,
+           36, 37, 39]]
 
 keycode = [macropad.Keycode.KEYPAD_SEVEN,
            macropad.Keycode.KEYPAD_EIGHT,
@@ -71,28 +81,28 @@ encoder_keycode = [macropad.Keycode.KEYPAD_PLUS,
                    macropad.Keycode.PERIOD,
                    macropad.Keycode.KEYPAD_EQUALS]
 
-button_mode = 0  # button_mode 0 for NumPad / button_mode 1 for BlackBox
+button_mode = 2  # button_mode 0 for NumPad / button_mode 1 for BlackBox / 2 is init mode
 
 def config_checkt(key_event):
     global config_mode
-    global BKG_COLOR
+    global BKGND_COLOR
     global PRESSED_COLOR
 
     if key_event.pressed:
         if key_event.key_number == 0:
             set_button_mode(0)
             config_mode = 0
-            BKG_COLOR = YELLOW
+            BKGND_COLOR = YELLOW
             PRESSED_COLOR = WHITE
             macropad.pixels.brightness = 0.05
-            set_pixel_colors()
+            set_pixel_color_mode()
         if key_event.key_number == 2:
             set_button_mode(1)
             config_mode = 0
-            BKG_COLOR = MAGENTA
+            BKGND_COLOR = MAGENTA
             PRESSED_COLOR = CYAN
             macropad.pixels.brightness = 0.05
-            set_pixel_colors()
+            set_pixel_color_mode()
 
 
 def set_button_mode(button_layout):
@@ -112,23 +122,20 @@ def key_maps(map):
     return key_map[map]
 
 
-def set_pixel_colors():
+def set_pixel_color_mode():
     for key in range(12):
-        if key == 9 and button_mode == 0:
-            macropad.pixels[key] = CYAN
-        elif key == 11 and button_mode == 0:
-            macropad.pixels[key] = MAGENTA
-        else:
-            macropad.pixels[key] = BKG_COLOR
+        set_background_colors(key)
 
 
-def reset_pixel_to_color_a(key):
+def set_background_colors(key):
     if key == 9 and button_mode == 0:
         macropad.pixels[key] = CYAN
     elif key == 11 and button_mode == 0:
         macropad.pixels[key] = MAGENTA
+    elif key in [2, 5, 8, 11] and row_4 == True:
+        macropad.pixels[key] = MINT
     else:
-        macropad.pixels[key] = BKG_COLOR
+        macropad.pixels[key] = BKGND_COLOR
 
 
 def set_button_mode_text():
@@ -139,6 +146,10 @@ def set_button_mode_text():
         text_lines = macropad.display_text("BlackBox MIDI")
         text_lines[0].text = f"{mode_text[encoder_mode]} {row[row_pos]}"
     return text_lines
+
+
+def reset_pixel_to_bkgnd_color(key):
+    set_background_colors(key)
 
 
 def send_keypad_click(key):
@@ -152,11 +163,27 @@ def send_encoder_click(encoder_pos):
         return macropad.keyboard.send(encoder_keycode[encoder_pos])
 
 
+def toggle_row():
+    global key_map
+    global row_4
+    if row[row_pos] == 4:
+        row_4 = True
+        key_map = row_map[1]
+        set_pixel_color_mode()
+    else:
+        row_4 = False
+        set_pixel_color_mode()
+        key_map = row_map[0]
+
 last_knob_pos = macropad.encoder  # store knob position state
 encoder_pos = 0
 encoder_mode = 0
-mode_text = [f"CC #{CC_NUM0}", f"CC #{CC_NUM1}", f"CC #{CC_NUM2}"]
+row_pos = 0
+mode_text = [f"CC # {CC_NUM0}:", f"CC #{CC_NUM1}:",
+             f"CC # {CC_NUM2}:", "Active row:"]
 cc_values = [0, 0, 0]  # initial CC values
+
+row_4 = False
 
 characters_entered = ""
 
@@ -182,7 +209,7 @@ while True:
                         characters_entered = ""
                 if key_event.released:
                     key = key_event.key_number
-                    reset_pixel_to_color_a(key)
+                    reset_pixel_to_bkgnd_color(key)
 
             if button_mode == 1:
                 if key_event.pressed:
@@ -193,7 +220,7 @@ while True:
                 if key_event.released:
                     key = key_event.key_number
                     macropad.midi.send(macropad.NoteOff(key_map[key], 0))
-                    reset_pixel_to_color_a(key)
+                    reset_pixel_to_bkgnd_color(key)
                     text_lines[1].text = ""
                     #text_lines[1].text = "SampleOff:{}".format(key_map[key])
 
@@ -210,6 +237,7 @@ while True:
             else:
                 characters_entered = f"{characters_entered}{encoder_map[encoder_pos]}"
             text_lines[1].text = f"{characters_entered}"
+            
         if button_mode == 1:
             encoder_mode = (encoder_mode+1) % 3
             text_lines[0].text = f"{mode_text[encoder_mode]} {int(cc_values[encoder_mode]*4.1)}"
