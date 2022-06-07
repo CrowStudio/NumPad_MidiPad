5af465944f53001e2645971fea44d4b4d1985231
CrowStudio <crowstudio@gmail.com>
2022-06-07 14:10:54 +0200

    Changed constant variables to static constant, changed mode_text to constant, and cleaned up some code

--- MidiCtrl.py
+++ MidiCtrl.py
@@ -8,20 +8,23 @@ class Sample:
     ON = "ON"
 
 class MidiCtrl:
+    CYAN = (0, 255, 255)
+    MAGENTA = (255, 0, 255)
+    MINT = (0, 255, 50)
+
+    NUMPAD_KEY_COLOR = MAGENTA
+    PRESSED_COLOR = CYAN
+
+    CC_NUM0 = 7  # Volume
+    CC_NUM1 = 10 # Pan
+    CC_NUM2 = 1  # Modulation Wheel
+
+    CC = [CC_NUM0, CC_NUM1, CC_NUM2]
+
+    MODE_TEXT = [f"CC # {CC_NUM0}:", f"CC #{CC_NUM1}:",
+                 f"CC # {CC_NUM2}:", "Active row:"]
+
     def __init__(self, macropad):
-        self.CYAN = (0, 255, 255)
-        self.MAGENTA = (255, 0, 255)
-        self.MINT = (0, 255, 50)
-
-        self.NUMPAD_KEY_COLOR = self.MAGENTA
-        self.PRESSED_COLOR = self.CYAN
-
-        self.CC_NUM0 = 7  # Volume
-        self.CC_NUM1 = 10 # Pan
-        self.CC_NUM2 = 1  # Modulation Wheel
-
-        self.CC = [self.CC_NUM0, self.CC_NUM1, self.CC_NUM2]
-
         self.macropad = macropad
         self.macropad_sleep = False
 
@@ -36,20 +39,20 @@ class MidiCtrl:
                           40, 41, 43,
                           36, 37, 39]]
 
-        self.latch_map = {0: [Sample.TOGGLE, Sample.OFF], 1: [Sample.MOM, Sample.OFF], 2: [Sample.MOM, Sample.OFF],
-                          3: [Sample.MOM, Sample.OFF], 4: [Sample.MOM, Sample.OFF], 5: [Sample.MOM, Sample.OFF],
-                          6: [Sample.MOM, Sample.OFF], 7: [Sample.MOM, Sample.OFF], 8: [Sample.MOM, Sample.OFF],
-                          9: [Sample.MOM, Sample.OFF], 10: [Sample.MOM, Sample.OFF], 11: [Sample.MOM, Sample.OFF]}
+        self.latch_map = {0: [Sample.TOGGLE, Sample.OFF], 1: [Sample.MOM, Sample.OFF],  2: [Sample.MOM, Sample.OFF],
+                          3: [Sample.MOM, Sample.OFF],    4: [Sample.MOM, Sample.OFF],  5: [Sample.MOM, Sample.OFF],
+                          6: [Sample.MOM, Sample.OFF],    7: [Sample.MOM, Sample.OFF],  8: [Sample.MOM, Sample.OFF],
+                          9: [Sample.MOM, Sample.OFF],   10: [Sample.MOM, Sample.OFF], 11: [Sample.MOM, Sample.OFF]}
 
         self.latch_row_3 = {2: [Sample.MOM, Sample.OFF],
                             5: [Sample.MOM, Sample.OFF],
                             8: [Sample.MOM, Sample.OFF],
-                            11: [Sample.MOM, Sample.OFF]}
+                           11: [Sample.MOM, Sample.OFF]}
 
         self.latch_row_4 = {2: [Sample.TOGGLE, Sample.OFF],
                             5: [Sample.MOM, Sample.OFF],
                             8: [Sample.MOM, Sample.OFF],
-                            11: [Sample.MOM, Sample.OFF]}
+                           11: [Sample.MOM, Sample.OFF]}
 
         self.row = [3, 4]
 
@@ -62,8 +65,7 @@ class MidiCtrl:
         self.encoder_mode = 3
         self.encoder_pos = 0
         self.row_pos = 0
-        self.mode_text = [f"CC # {self.CC_NUM0}:", f"CC #{self.CC_NUM1}:",
-                          f"CC # {self.CC_NUM2}:", "Active row:"]
+
         self.cc_values = [0, 0, 0]  # initial CC values
 
         self.row_4 = False
@@ -72,7 +74,7 @@ class MidiCtrl:
 
     def send_key_press(self, key_event, text_lines):
         key = key_event.key_number
-        self.macropad.pixels[key] = self.PRESSED_COLOR
+        self.macropad.pixels[key] = MidiCtrl.PRESSED_COLOR
         self.macropad.midi.send(self.macropad.NoteOn(self.key_map[key], 120))
         text_lines[2].text = f"SampleOn:{self.key_map[key]}"
         return time.monotonic()
@@ -92,27 +94,27 @@ class MidiCtrl:
             self.cc_values[self.encoder_mode] = min(
                 max(self.cc_values[self.encoder_mode] + self.knob_delta, 0), 127)
             self.__send_cc_value(self.encoder_mode)
-            text_lines[0].text = f"{self.mode_text[self.encoder_mode]} {int(self.cc_values[self.encoder_mode])}"
+            text_lines[0].text = f"{MidiCtrl.MODE_TEXT[self.encoder_mode]} {int(self.cc_values[self.encoder_mode])}"
         else:
             self.row_pos = self.macropad.encoder % 2
             self.__toggle_row()
             self.init_row = False
-            text_lines[0].text = f"{self.mode_text[self.encoder_mode]} {self.row[self.row_pos]}"
+            text_lines[0].text = f"{MidiCtrl.MODE_TEXT[self.encoder_mode]} {self.row[self.row_pos]}"
         self.last_knob_pos = self.macropad.encoder
         return time.monotonic()
 
 
     def __send_cc_value(self, num):
         self.macropad.midi.send(self.macropad.ControlChange(
-            self.CC[num], int(self.cc_values[self.encoder_mode])))
+            MidiCtrl.CC[num], int(self.cc_values[self.encoder_mode])))
 
 
     def handle_encoder_click(self, text_lines):
         self.encoder_mode = (self.encoder_mode + 1) % 4
         if self.encoder_mode in [0, 1, 2]:
-            text_lines[0].text = f"{self.mode_text[self.encoder_mode]} {int(self.cc_values[self.encoder_mode])}"
+            text_lines[0].text = f"{MidiCtrl.MODE_TEXT[self.encoder_mode]} {int(self.cc_values[self.encoder_mode])}"
         else:
-            text_lines[0].text = f"{self.mode_text[self.encoder_mode]} {self.row[self.row_pos]}"
+            text_lines[0].text = f"{MidiCtrl.MODE_TEXT[self.encoder_mode]} {self.row[self.row_pos]}"
         return time.monotonic()
 
 
@@ -148,9 +150,9 @@ class MidiCtrl:
 
     def __set_background_colors(self, key):
         if key in [2, 5, 8, 11] and self.row_4 == True:
-            self.__check_toggle_state(key, self.MINT)
+            self.__check_toggle_state(key, MidiCtrl.MINT)
         else:
-           self.__check_toggle_state(key, self.NUMPAD_KEY_COLOR)
+           self.__check_toggle_state(key, MidiCtrl.NUMPAD_KEY_COLOR)
 
 
     def __reset_pixel_to_bkgnd_color(self, key):
@@ -160,7 +162,7 @@ class MidiCtrl:
     def __check_toggle_state(self, key, COLOR):
         if self.init_row == True:
             if self.latch_map[key][0] == Sample.TOGGLE and self.latch_map[key][1] == Sample.ON:
-                self.macropad.pixels[key] = self.PRESSED_COLOR
+                self.macropad.pixels[key] = MidiCtrl.PRESSED_COLOR
             else:
                 self.macropad.pixels[key] = COLOR
         elif self.latch_map[key][0] == Sample.TOGGLE and self.latch_map[key][1] == Sample.OFF:
